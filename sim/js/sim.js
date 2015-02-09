d4e9e0b0d4ecdee96605e3ffc06481505bbdf08c
vahansoft <vahansoft@gmail.com>
2015-02-09 18:26:52 +0400

    Fountain Simulator Fixed

--- sim/js/sim.js
+++ sim/js/sim.js
@@ -9,7 +9,7 @@ function rgbToHex(r, g, b) {
     if (g.length == 1) g = '0' + g;
     if (b.length == 1) b = '0' + b;
 
-    return (r + g + b).toUpperCase();
+    return "#" + (r + g + b).toUpperCase();
 }
 
 
